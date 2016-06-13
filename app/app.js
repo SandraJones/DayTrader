8439c68e6019f01cb0652fe2c9e4f0f5738df981
Sandy Jones <sandynashv@gmail.com>
2016-06-13 17:28:23 -0500

    cleared our files for authentication

--- app/app.js
+++ app/app.js
@@ -3,52 +3,46 @@
 var app = angular.module("DayTrader", ["ngRoute"])
   .constant("firebaseURL", "https://sjdaytrader.firebaseio.com/")
 
+    let isAuth = (AuthFactory) => new Promise ((resolve, reject) => {
+      if(AuthFactory.isAuthenticated()){
+        console.log("User is authenticated, resolve route promise");
+        resolve();
+      } else {
+        console.log("User is not authenticated, reject route promise");
+        reject();
+      }
+    })
 
-let isAuth = (AuthFactory) => new Promise ((resolve, reject) => {
-  if(AuthFactory.isAuthenticated()){
-    console.log("User is authenticated, resolve route promise");
-    resolve();
-  } else {
-    console.log("User is not authenticated, reject route promise");
-    reject();
-  }
-})
-
-app.directive('errSrc', function() {
-  return {
-    link: function(scope, element, attrs) {
-      element.bind('error', function() {
-        if (attrs.src !== attrs.errSrc) {
-          attrs.$set('src', attrs.errSrc);
+    app.directive('errSrc', function() {
+      return {
+        link: function(scope, element, attrs) {
+          element.bind('error', function() {
+            if (attrs.src !== attrs.errSrc) {
+              attrs.$set('src', attrs.errSrc);
+            }
+          });
         }
-      });
-    }
-  };
-});
-  
-
-app.config(function($routeProvider){
-	$routeProvider.
-		when('/', {
-			templateUrl: 'partials/login',
-			controllers: 'LoginCtrl.js'
-		}).
-    	otherwise('/',{
-    	templateUrl: 'partials/login',
-			controllers: 'LoginCtrl.js'	
+      };
     });
-   
-     app.run(($location) =>{
-  	 let addressRef = new Firebase("https://sjdaytrader.firebaseio.com/");
 
-  	 addressRef.unauth();
+    app.config(function($routeProvider){
+    	$routeProvider.
+    		when('/', {
+    			templateUrl: 'partials/login',
+    			controller: 'LoginCtrl.js'
+    	  })
+    });
+       
+    app.run(($location) =>{
+	  let addressRef = new Firebase("https://sjdaytrader.firebaseio.com/");
+
+	  addressRef.unauth();
 
     //When Registering:
-  	addressRef.onAuth(authData =>{
-   	 if(!authData){
-       $location.path("/login");
-     }
+	  addressRef.onAuth(authData =>{
+ 	    if(!authData){
+        $location.path("/login");
+      }
     })	
-  });
+    });
 
-});
