ffb7fe337ef43810eaa4398fd67751e3ef44edfc
Sandy Jones <sandynashv@gmail.com>
2016-06-24 21:22:50 -0500

    deleting file

--- app/controllers/SearchCtrl.js
+++ app/controllers/SearchCtrl.js
@@ -3,16 +3,29 @@ app.controller('SearchCtrl', ["$scope", "$location", "StockFactory",
 	 function($scope, $location, StockFactory){
 	 	$scope.getAStock;
 	 	$scope.stockArray;
-	 	$scope.getKeys;
-	 	$scope.stockObj = {
-	 		Name: "",
-	 		Dates: {}
-	 	};
-
+	 	$scope.stockFave;
 
 	 	$scope.searchAStock = function(){
 	 		StockFactory.getStocks($scope.getAStock).then(function(stockReturn){
 	 			$scope.stockArray = stockReturn.dataset.data[0];
 	 		});
 	 	}
+
+	 	// $scope.addAStock = function(stockArray){
+	 	//  	$scope.stockFave = returnedFave;
+	 	// 		function toObject(stockFave) {
+  	// 	// 		var rv = {};
+  		// 			for (var i = 0; i < $stock.stockFave.length; ++i){
+    // 					if (stockFave[i] !== undefined){ 
+    // 						rv[i] = stockFave[i];
+  		// 					return rv;
+				// 			}
+  		// 				console.log(rv);
+	 	 // 				}
+				// .success(
+				// function(objectF){
+				// resolve(objectF);
+				// });
+		// 		};
+		// };
 }]);
